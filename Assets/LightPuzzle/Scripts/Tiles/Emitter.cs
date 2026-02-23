a9d7a0ceffdeb7f4fb6d15b8ce4a5a7b16bd1274
James Mills <jcmills.developer@gmail.com>
2026-02-23 15:49:12 +1100

    Setup for light beams

---
+++ Assets/LightPuzzle/Scripts/Tiles/Emitter.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace LightPuzzle.Tiles
+{
+	public class Emitter : Tile
+	{
+		[SerializeField] private Direction emissionDirection = Direction.East;
+
+		public void SetActive(bool active)
+		{
+			
+		}
+	}
+}
